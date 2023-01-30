013bb1b98d6f00136cb46938d41ffb3cfaab6171
diazAaron <dbaaron@live.com>
2023-01-30 12:55:29 +0100

    cambios

--- ModeracionArticulos/MainWindowVM.cs
+++ ModeracionArticulos/MainWindowVM.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Mvvm.Input;
+﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 
 namespace ModeracionArticulos
@@ -17,42 +14,40 @@ namespace ModeracionArticulos
     public class MainWindowVM
     {
         private readonly string _apiKey = "4001e1e5b6094f71a6af15cde53d3a68";
-        private readonly string _endpoint = "westeurope.api.cognitive.microsoft.com";
-
-        /*public ContentModerator(string apiKey, string endpoint)
-        {
-            _apiKey = apiKey;
-            _endpoint = endpoint;
-        }*/
+        private readonly string _endpoint = "https://westeurope.api.cognitive.microsoft.com";
 
         public async void ModerateText(string text)
         {
-            using (var client = new HttpClient())
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+
+            queryString["autocorrect"] = "true";
+            queryString["PII"] = "true";
+            //queryString["listId"] = "{string}";
+            queryString["classify"] = "True";
+            queryString["language"] = "spa";
+            var uri = "https://westeurope.api.cognitive.microsoft.com/contentmoderator/moderate/v1.0/ProcessText/Screen?" + queryString;
+
+            HttpResponseMessage response;
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(text);
+
+            using (var content = new ByteArrayContent(byteData))
             {
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
-                //var uri = $"{_endpoint}/text/moderation/v1.0/Screen?autocorrect=false&PII=false&listId=<listId>&language=eng";
-                var uri = $"{_endpoint}/text/moderation/v1.0/Screen";
-
-                var content = new StringContent(JsonConvert.SerializeObject(new { text }), Encoding.UTF8, "application/json");
-                var response = await client.PostAsync(uri, content);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine("Moderation failed with status code: " + response.StatusCode);
-                    return;
-                }
-
-                var moderationResult = JsonConvert.DeserializeObject<ModerationResult>(await response.Content.ReadAsStringAsync());
-                Console.WriteLine("Moderation status: " + moderationResult.Status);
+                content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+                response = await client.PostAsync(uri, content);
             }
+            Console.WriteLine(response);
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine(response.Content);
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine(response.Headers);
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine(response.StatusCode);
         }
 
-        private class ModerationResult
-        {
-            public string Status { get; set; }
-        }
     }
-
-
 }
 
